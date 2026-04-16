47dce8a168bcaeeffc2b0bf449f9bd4596b9c147
MaxC25 <7951@balcarras.gloucs.sch.uk>
2026-04-16 13:19:29 +0000

    Fixed a minor bug with handling spaces

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const imps=["→","->","⇒","⊃"]//implication then its reverse
 const pmis=["←","<-","⇐","⊂"]
 const equals=["≡","=="]
 const parens="()"
+const correctible=["->","&","==","|","~","E","N","V","⊕'"]
 Array.prototype.last=function(){return this[this.length-1]}
 function tokenise(expr){/*
 _____
@@ -40,27 +41,28 @@ function prec(op){switch(op){//should probably change it to something more flexi
     case"≡":return 0xFE
 }}
 function shunt(orders){//the shunting yard
-let [out,ops,t]=[[],[],Uint8Array];//t is a single byte representing type where 0:undeclared 1:prefix 2:infix 3:suffix 4:bracket 5:literal_or_reference
+let [out,ops,t,normals]=[[],[],Uint8Array,new Set("&|⊕V↓→←≡".split(""))];//t is a single byte representing type where 0:undeclared 1:prefix 2:infix 3:suffix 4:bracket 5:literal_or_reference
 while(orders.length){
 let order=orders.pop();
-if(order===""){order=orders.pop()}
+while((order==="")||/^\s*$/.test(order)){order=orders.pop()}
 if(orders.last()===""){orders.pop()}
 t[0]=false
-let TF=(T,F)=>F?(orders.last()==="'"?(orders.pop(),F):T):orders.last()==="'"?(T+orders.pop):T;//normalises and deals with negated operators
-if(["|","¦","↑"].includes(order)){orders.last()===order?(()=>order+=orders.pop(),order=(ors.slice(1,3).includes(order)?TF("V","↓"):TF("↓","V"))):order=TF(order,"|","&");t[0]=2}//deals with weird non-prefix nature of ¦¦, also this is the only bit of old code left as it was the only bit known to work.
+let TF=(T,F)=>F?(orders.last()==="'"?(orders.pop(),F):T):orders.last()==="'"?(T+orders.pop()):T;//normalises and deals with negated operators
+if(["|","¦","↑"].includes(order)){orders.last()===order?(()=>order+=orders.pop(),(orders.last()===""?orders.pop():null),order=(ors.slice(1,3).includes(order)?TF("V","↓"):TF("↓","V"))):order=TF(order,"|","&");t[0]=2}//deals with weird non-prefix nature of ¦¦, also this is the only bit of old code left as it was the only bit known to work.
 else{//normalises
 ["1","0"].includes(order)?out.push(Boolean(order)):
 order==="("?ops.push(order):
 order==="'"?out.push("'"):
 prenots.includes(order)?ops.push("~"):
+(orders.last()!=="'")&&normals.has(order)?t[0]=2:
 ands.includes(order)?[t[0],order]=[2,TF("&",'|')]:
 nands.includes(order)?[t[0],order]=[2,TF("|","&")]:
 xors.includes(order)?[t[0],order]=[2,TF("⊕")]:
-ors.includes(order)?[t[0],order]=[2,TF("V","↓")]:
+[ors[1],...ors.slice(2)].includes(order)?[t[0],order]=[2,TF("V","↓")]:
 nors.includes(order)?[t[0],order]=[2,TF("↓","V")]:
-imps.includes(order)?[t[0],order]=[2,TF("→",)]:
-pmis.includes(order)?[t[0],order]=[2,TF("←",)]:
-equals.includes(order)?[t[0],order]=[2,TF("≡",)]://end of normalisation.
+imps.includes(order)?[t[0],order]=[2,TF("→")]:
+pmis.includes(order)?[t[0],order]=[2,TF("←")]:
+equals.includes(order)?[t[0],order]=[2,TF("≡")]://end of normalisation.
 order==="="?t[0]=2:out.push(order)
 }
 if(t[0]==2){while(ops.length&&ops.last()!="("&&((prec(ops.last())>prec(order))||((prec(order)===prec(ops.last))&&(["→".has(order)])))){out.push(ops.last())}ops.push(order)}
@@ -70,7 +72,17 @@ while(ops.length&&ops.last!="("){out.push(ops.pop())}
 if(ops.last==="("){console.error("mismatched brackets")}
 return out
 }
-
+function RPNpedant(a){
+switch(a){
+  case "~":case "N": return "'";
+  case "V":return "A";
+  case "&":return "K";
+  case "|":return "D";
+  case "->":return "C";
+  case"E":case "==":case"⊕'":return "Q";
+}
+}
+function RPNlike(a){if(a==="~"){return "'"}}//not true
 document.getElementById("evalBtn").addEventListener("click", () => {
     //bitwise=false
     //console.log(tokenise(document.getElementById("in").value))
