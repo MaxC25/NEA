4fd5dd6318c7ae791df7383f874115fc7efa9dd8
MaxC25 <7951@balcarras.gloucs.sch.uk>
2026-05-10 22:01:09 +0000

    The interpreter works

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//import "node_modules/@bitarray/es6/dist/esm/src/bitarray.js"//replace with your installation site of 
+//import BitArray from "/node_modules/@bitarray/es6/dist/esm/src/bitarray.js"//allows bitarray, replace if desired
 // Encoding: UTF-8
 //this will not work with browsers not supporting ES6 because I like local variables.
-//document parts
-//const expressionInput = document.getElementById("in");
-//const evalBtn = document.getElementById("evalBtn")
-//var bitwise=false
-var vars=[]
+const OUTBOX=document.getElementById("out")
+try{Number.prototype&=0xFF}catch{}//try to make number.prototype 8 bit.
+const [C,CC]=[((p,q)=>~p|q),((p,q)=>!p||q)]//imply in RPN, duplication to mean logical
+const [L,LL]=[((p,q)=>p&~q),((p,q)=>p&&!q)]//nimply, note: incorrect notation used for or in LL
 const prenots="!~¬"//for all that are not in square brackets, please do .split("")
 //const postnot="'" commented out for being pointless but being good for documentation
+const $=x=>k=>$(k(x))//useful chaining omniparic stuff
 const ands="&*·ϗ⁊∧∩"
 const nands="|¦↑⊽"
 const xors=["⊕","XOR","^"]
@@ -18,7 +18,9 @@ const pmis=["←","<-","⇐","⊂"]
 const equals=["≡","=="]
 const parens="()"
 const correctible=["->","&","==","|","~","E","N","V","⊕'"]
+const IsNum=a=>(a===!!a)||(typeof a==="number")//checks if of number-like-type
 Array.prototype.last=function(){return this[this.length-1]}
+Iterator.prototype.last=function(){return this[this.length-1]}
 //imply(a,b)=!a||b
 function tokenise(expr){/*
 _____
@@ -90,20 +92,71 @@ switch(a){
   case "|":return "D";
   case "&":return "K";
   case "->":case "→":return "C";
+  case "->'":case "→'":return "L";
+  case "<-":case "←":return "B";
+  case "<-'": case"←'":return "M"
   case "=='":case "⊕":return "J";
   case "E":case "==":case "⊕'":return "Q";
   default:return a
 }
 }
-/*
-const correctRPN=new set("'AXDJCJQ".split(""))
-function compile(RPN,variables){
-while RPN.length()
-}*/
-//function RPNlike(a){if(a==="~"){return "'"}}//not true
+
+function interpret(RPN,Bwise,vars){//bitwise is an ugly solution for lack of unification between bitwise and logical operators
+let out=[];
+RPN.reverse();//I will pretend it isn't and pop pops from the start. So now last is first and first is last becuse unshift is slow in V8.
+let op
+  while(RPN.length){
+  op=RPNpedant(RPN.pop())
+  if(IsNum(op)){out.push(op)}
+  else{
+  let tmp=[]//this is for temporary storage of values
+  switch(op){//pipelines with the corrector
+    case("'"):out.push(Bwise?~out.pop():!out.pop());break;
+    case("A"):out.push(Bwise?out.pop()|out.pop():out.pop()||out.pop());break;//or
+    case("B"):tmp.push(out.pop());out.push(Bwise?C(tmp[0],out.pop()):CC((tmp[0],out.pop())));break;//converse
+    case("C"):tmp.push(out.pop());out.push(Bwise?C(out.pop(),tmp[0]):CC((out.pop(),tmp[0])));break;//imply, o[0] should really be o[1] for the next 3 lines but optimisation.
+    case("D"):out.push(Bwise?~(out.pop()&out.pop()):!(out.pop()&&out.pop()));break;//nand
+    case("E"):out.push(Bwise?~(out.pop()^out.pop()):out.pop()==out.pop());break//
+    case("J"):Bwise?out[out.length-2]^=out.pop():out.push(out.pop()!=out.pop());//XOR
+    case("K"):Bwise?out[out.length-2]&=out.pop():out[out.length-2]&&=out.pop();break;//and
+    case("L"):tmp.push(out.pop());push(L(out.pop(),tmp[0]),LL((out.pop(),tmp[0])));break;//nimply
+    case("M"):tmp.push(out.pop());push(L(tmp[0],out.pop()),LL((tmp[0],out.pop())));break;//ylpmin
+    default:try{op.push(vars.get(op.pop()))}catch{throw "non existant value or operator"}
+    }
+  }
+}
+return out.pop()
+}
+
+document.getElementById("RPN").addEventListener("click", () => {
+    //bitwise=false
+    //console.log(tokenise(document.getElementById("in").value))
+    OUTBOX.innerHTML=(shunt(tokenise(document.getElementById("in").value)).map(a=>RPNpedant(a)).join(" "))
+})
 document.getElementById("evalBtn").addEventListener("click", () => {
     //bitwise=false
     //console.log(tokenise(document.getElementById("in").value))
-    console.log(shunt(tokenise(document.getElementById("in").value),true).join(" "))
-    console.log(["N","V","↓","|","&","->","→","=='","E","==","⊕'","'","X","D","K","C","J","Q"].map((x)=>RPNpedant(x)))
+    let a=interpret(shunt(tokenise(document.getElementById("in").value)),document.getElementById("Bwise").checked)
+    OUTBOX.innerHTML=a===!!a?a&1:a.tostring(2)
+    let b=(a)=>(b)=>a+b
+    b=$(-b)
+    console.log(b(1)(2))
 })
+/*
+function test() {
+  const outputArea = document.getElementById("out")
+  //blank the area
+  a=[]
+  //build a table
+
+  let table = document.createElement("table")
+  let thead = document.createElement("thead")
+
+  table.appendChild(thead)
+
+
+  //at the end!
+  OUTBOX.innerHTNL=a.pop
+
+}
+*/
